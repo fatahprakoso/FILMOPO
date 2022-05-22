dfb17e73ee4bc1589f05db6f4d9a729933c75c37
RayhaaNako <rayhaanimtiyaz1301@gmail.com>
2022-05-22 21:41:28 +0700

    add: breeze

---
+++ database/migrations/2022_05_22_141251_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id('movie_id');
+            $table->string('name',250);
+            $table->int('length');
+            $table->date('release_dt');
+            $table->string('poster',255);
+            $table->string('rated');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
