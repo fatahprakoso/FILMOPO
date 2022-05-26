28424c29baf379261cdc8764bb531d79d9dd5a12
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-05-26 14:03:06 +0700

    database/

--- database/migrations/2022_05_23_130812_create_movie_actors_table.php
+++ database/migrations/2022_05_23_130812_create_movie_actors_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('movie_actors', function (Blueprint $table) {
-            // $table->id();
-            // $table->timestamps();
             $table->unsignedBigInteger('movie_id');
             $table->unsignedBigInteger('actor_id');
             $table->foreign('movie_id')->references('movie_id')->on('movies')->onDelete('cascade');
