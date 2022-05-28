6fd5f8d8f3f353cee7f88c0ed4c9b5105842f9e4
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-05-28 11:33:17 +0700

    update redesign login form using bootstrap and vanta.js

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,76 @@
 <x-guest-layout>
     <x-auth-card>
 
-        <section class="vh-100" style="background-color: #9A616D;">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col col-xl-10">
-                        <div class="card" style="border-radius: 1rem;">
-                            <div class="row g-0">
-                                <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="{{('poster_money-heist.jpg')}}" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                                </div>
-                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div class="card-body p-4 p-lg-5 text-black">
+        <style>
+            .btn-submit-login {
+                width: 170px;
+            }
 
-                                        <h1 style="font-weight: bold;">FILMOPO</h1>
-                                        <h5>Login</h5>
+            @media screen and (max-width: 780px) {
+                .poster {
+                    justify-content: center;
+                }
 
-                                        <x-slot name="logo">
-                                            <a href="/">
-                                                <a class="navbar-brand ms-3" href="{{ route('home') }}" style="font-weight: 900; color: #000; font-size: 1.75rem; letter-spacing: 0.6vw; font-family: 'Bebas Neue', cursive;">
-                                                    <spin>FILM</spin>
-                                                    <spin style="color: red;">OPO</spin>
-                                                </a>
-                                            </a>
-                                        </x-slot>
+                .btn-submit-login {
+                    width: auto;
+                }
+            }
+
+        </style>
+
+        <x-slot name="logo">
+            <a href="/">
+                <a class="navbar-brand ms-3" href="{{ route('home') }}"
+                    style="font-size:2.5rem;font-weight: 800; font-family: 'Bebas Neue', cursive;">
+                    <span style="color: red">FILM</span><span
+                        style="color:white; background-color: red; padding:0.03em; margin-left: 0.05em;box-sizing: border-box;">OPO</span>
+                </a>
+            </a>
+        </x-slot>
+
+        <section class="bg-vanta" style="background-color: #03111f; height: calc(100% + 100vh)">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"></script>
+            <script>
+                VANTA.WAVES({
+                    el: ".bg-vanta",
+                    mouseControls: true,
+                    touchControls: true,
+                    gyroControls: false,
+                    minHeight: 200.00,
+                    minWidth: 200.00,
+                    scale: 1.00,
+                    scaleMobile: 1.00,
+                    color: 0x03111f,
+                    shininess: 20.00,
+                    waveHeight: 22.00,
+                    waveSpeed: 0.75,
+                    zoom: 0.81
+                })
+            </script>
+
+            <div class="container py-5">
+                <div class="card" style="border-radius: 1rem;">
+                    <div class="col">
+                        <div class="row g-1 justify-content-around">
+                            <div class="col-md-5 d-flex poster" style="height: 90vh;">
+                                <img src="{{ 'poster_money-heist.jpg' }}" alt="login form" class="img-fluid"
+                                    style="border-radius: 1rem 1rem; height:100%; object-fit: cover" />
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                                <div
+                                    class="  card-body p-4 p-lg-5 text-black d-flex flex-column justify-content-center align-items-center">
+
+                                    <div class="form-container" style="width: calc(10rem + 15vw)">
+
+                                        <div class="head-form">
+                                            <h1
+                                                style="font-size:4rem;font-weight: 900; font-family: 'Bebas Neue', cursive;">
+                                                <span style="color: red">FILM</span><span
+                                                    style="color:white; background-color: red; padding:0.025em; margin-left: 0.075em;box-sizing: border-box">OPO</span>
+                                            </h1>
+                                            <h5>Login</h5>
+                                        </div>
 
                                         <!-- Session Status -->
                                         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -35,30 +82,42 @@
                                             @csrf
 
                                             <!-- Email Address -->
-                                            <div>
-                                                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                            <div class="mt-3">
+                                                <div class="input-group">
+                                                    <x-input id="email" class="block mt-1 w-full form-control"
+                                                        type="email" name="email" :value="old('email')" required
+                                                        autofocus />
+                                                </div>
                                             </div>
                                             <x-label for="email" :value="__('Email address')" />
 
                                             <!-- Password -->
                                             <div class="mt-4">
-                                                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                                                <div class="input-group">
+                                                    <x-input id="password" class="form-control block mt-1 w-full"
+                                                        type="password" name="password" required
+                                                        autocomplete="current-password" />
+                                                </div>
                                             </div>
                                             <x-label for="password" :value="__('Password')" />
 
                                             <!-- Remember Me -->
                                             <div class="block mt-4">
                                                 <label for="remember_me" class="inline-flex items-center">
-                                                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                                                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                                    <input id="remember_me" type="checkbox"
+                                                        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                        name="remember">
+                                                    <span
+                                                        class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                                                 </label>
                                             </div>
 
-                                            <div class="flex items-center justify-end mt-4">
+                                            <div class="d-flex flex-column items-center justify-end mt-4">
                                                 @if (Route::has('password.request'))
-                                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                                                    {{ __('Forgot your password?') }}
-                                                </a>
+                                                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                                                        href="{{ route('password.request') }}">
+                                                        {{ __('Forgot your password?') }}
+                                                    </a>
                                                 @endif
 
                                                 <x-button class="ml-3">
@@ -66,8 +125,6 @@
                                                 </x-button>
                                             </div>
                                         </form>
-
-
                                     </div>
                                 </div>
                             </div>
@@ -75,6 +132,7 @@
                     </div>
                 </div>
             </div>
+
         </section>
 
     </x-auth-card>
